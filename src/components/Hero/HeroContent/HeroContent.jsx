0c413f5c42acfb207e9aa7b89e90ea2562f069dd
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-29 02:32:30 +0600

    Create an awesome button and fixed hero contents

--- src/components/Hero/HeroContent/HeroContent.jsx
+++ src/components/Hero/HeroContent/HeroContent.jsx
@@ -2,18 +2,25 @@ import Button from "../../Shared/Button/Button";
 
 export default function HeroContent() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center p-5">
-      <p>IT&prime;S TIME TO GET</p>
-      <h2>
-        Power<span>House</span> gym
-      </h2>
-      <p>
+    <div className="h-full flex flex-col gap-4 lg:gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-2 md:gap-4 ">
+        <p className="font-bold">IT&prime;S TIME TO GET</p>
+        <h2 className="uppercase font-semibold text-xl md:text-4xl lg:text-5xl">
+          <span className="text-blue-400">Power</span> House{" "}
+          <span className="text-blue-400">gym</span>
+        </h2>
+      </div>
+
+      <p className="text-sm md:text-base font-light">
         I hereby acknowledgement that I may become{" "}
-        <span>unbelievably powerhouse</span> and accept all risks of becoming
-        the local <span> mass montrosity</span>, afflicted with severe body
-        dismorphia, unable to fit through doors.
+        <span className="text-blue-400 font-medium">
+          unbelievably powerhouse
+        </span>{" "}
+        and accept all risks of becoming the local{" "}
+        <span className="text-blue-400 font-medium"> mass montrosity</span>,
+        afflicted with severe body dismorphia, unable to fit through doors.
       </p>
-      <Button text="heoo" />
+      <Button text="Accept & Begin" />
     </div>
   );
 }
--- src/components/Shared/Button/Button.jsx
+++ src/components/Shared/Button/Button.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
 
 export default function Button({text}) {
   return (
-    <div>{text}</div>
+    <button className='px-8 py-4 border-2 rounded-md border-blue-400 border-solid bg-slate-950  font-semibold duration-200'>
+    <p>{text}</p>
+    </button>
   )
 }
