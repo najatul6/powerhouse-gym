30d8aa26b33f5b4be4bae4f06482a4cb4b4f40ba
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-29 18:53:21 +0600

    Create box shadow for button with custom css

--- src/components/Shared/Button/Button.jsx
+++ src/components/Shared/Button/Button.jsx
@@ -1,7 +1,7 @@
 
 export default function Button({text}) {
   return (
-    <button className='px-8 py-4 border-2 rounded-md border-blue-400 border-solid bg-slate-950  font-semibold duration-200'>
+    <button className='px-8 py-4 border-2 rounded-md border-blue-400 border-solid bg-slate-950 font-semibold duration-200 blueShadow'>
     <p>{text}</p>
     </button>
   )
