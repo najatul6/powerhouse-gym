c5a97973973be64e6a97b3d4e5e84c49044fe6b8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-01 23:57:48 +0600

    call workout in app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react"
 import Generator from "./components/Generator/Generator"
 import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
 import WorkOur from "./components/WorkOut/WorkOur"
 
 
 function App() {
-  
+  const[workout, setWorkOut] =useState(null)
 
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
