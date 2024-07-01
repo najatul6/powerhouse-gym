51397d1333a26fd1bd5bf29c1cb82f34c5d687d2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-01 23:59:58 +0600

    send props via workout

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Header from "./components/Header/Header";
 import WorkOur from "./components/WorkOut/WorkOur";
 
 function App() {
-  const [workout, setWorkOut] = useState(null);
+  const [workout, setWorkout] = useState(null);
 
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Header />
 
       <Generator />
-      {workout && <WorkOur />}
+      {workout && <WorkOur workout={workout}/>}
     </main>
   );
 }
