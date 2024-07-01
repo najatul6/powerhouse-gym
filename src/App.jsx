8f0abf849bd0066cc2a2aa6959813b11861108ea
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-01 23:59:17 +0600

    set condition for workout section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from "react"
-import Generator from "./components/Generator/Generator"
-import Header from "./components/Header/Header"
-import WorkOur from "./components/WorkOut/WorkOur"
-
+import { useState } from "react";
+import Generator from "./components/Generator/Generator";
+import Header from "./components/Header/Header";
+import WorkOur from "./components/WorkOut/WorkOur";
 
 function App() {
-  const[workout, setWorkOut] =useState(null)
+  const [workout, setWorkOut] = useState(null);
 
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
-     <Header/>
-      
-      <Generator/>
-      <WorkOur/>
-      
+      <Header />
+
+      <Generator />
+      {workout && <WorkOur />}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
