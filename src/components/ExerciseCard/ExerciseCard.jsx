642f418fd42f3ba0ec2a36ef66e3c8753308dd0b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-02 20:12:09 +0600

    complete ui design for muscle group

--- src/components/ExerciseCard/ExerciseCard.jsx
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -6,10 +6,14 @@ export default function ExerciseCard({ exercise, i }) {
         <h4 className="text-3xl sm:text-4xl md:text-5xl font-semibold hidden sm:inline text-slate-400">
           0{i + 1}
         </h4>
-        <h2 className="capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 md:text-center">
+        <h2 className="capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center">
           {exercise.name.replaceAll("_", " ")}
         </h2>
         <p className="text-sm text-slate-400 capitalize">{exercise.type}</p>
+      </div>
+      <div className="flex flex-col">
+        <h3 className="text-slate-400 text-sm">Muscle Group</h3>
+        <p className="capitalize">{exercise.muscles.join(' & ')}</p>
       </div>
     </div>
   );
